33cd48356ddf65575b6c4ce43f9f099ff7d101b9
Razvan <not.obelix@gmail.com>
2022-10-22 16:13:07 +0300

    installed gun and wagmi + basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
+import { configureChains, chain } from "wagmi";
+import { publicProvider } from "wagmi/providers/public";
+import { WagmiConfig, createClient } from "wagmi";
+
+const { provider, webSocketProvider } = configureChains(
+  [chain.mainnet],
+  [publicProvider()]
+);
+
+const client = createClient({
+  provider,
+  webSocketProvider,
+});
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig client={client}>
+      <Component {...pageProps} />
+    </WagmiConfig>
+  );
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import type { NextPage } from "next";
+import { useAccount, useConnect, useEnsName } from "wagmi";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
 const Home: NextPage = () => {
-  return <>Start</>;
+  const { address, isConnected } = useAccount();
+  const { data: ensName } = useEnsName({ address });
+  const { connect } = useConnect({
+    connector: new MetaMaskConnector(),
+  });
+
+  if (isConnected) return <div>Connected to {ensName ?? address}</div>;
+  return <button onClick={() => connect()}>Connect Wallet</button>;
 };
 
 export default Home;
