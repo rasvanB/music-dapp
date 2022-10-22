e49894cb2821b19bd1a91fd6087f130b91964406
Razvan <not.obelix@gmail.com>
2022-10-22 19:58:51 +0300

    removed injected connector

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,15 +1,12 @@
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { InjectedConnector } from "wagmi/connectors/injected";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { Connector } from "wagmi";
 
-type connectors = "injected" | "metamask" | "coinbase" | "walletconnect";
+type connectors = "metamask" | "coinbase" | "walletconnect";
 
 export const getConnector = (connectorName: connectors): Connector => {
   switch (connectorName) {
-    case "injected":
-      return new InjectedConnector();
     case "metamask":
       return new MetaMaskConnector();
     case "coinbase":
