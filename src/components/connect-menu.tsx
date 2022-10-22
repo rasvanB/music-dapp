3681478c9816a3c733607e51a2bc2b69dd950f7f
Razvan <not.obelix@gmail.com>
2022-10-22 22:08:40 +0300

    added connect menu logic

--- src/components/connect-menu.tsx
+++ src/components/connect-menu.tsx
@@ -1,25 +1,54 @@
-type MenuOptionProps = {
-  text: string;
-};
+import Image from "next/image";
+import { useConnect } from "wagmi";
+import { connectors, getConnector } from "../utils/wagmi";
 
-const ConnectMenuOption = ({ text }: MenuOptionProps) => {
+type MenuOptionProps = {
+  text: connectors;
+} & React.BaseHTMLAttributes<HTMLDivElement>;
+
+const ConnectMenuOption = ({ text, onClick }: MenuOptionProps) => {
+  const { connect } = useConnect({ connector: getConnector(text) });
   const connectorName =
     text === "metamask"
       ? "MetaMask"
       : text === "coinbase"
-      ? "Coinbase"
+      ? "Coinbase Wallet"
       : text === "walletconnect"
       ? "WalletConnect"
       : "";
-  return <div>{connectorName}</div>;
+  return (
+    <div
+      className="mt-3 flex cursor-pointer items-center"
+      onClick={(event) => {
+        connect();
+        if (onClick) onClick(event);
+      }}
+    >
+      <Image
+        src={`/${text}.png`}
+        alt="wallet logo"
+        width="35px"
+        height="35px"
+      ></Image>
+      <div className="pl-3 font-semibold tracking-wider text-gray-200">
+        {connectorName}
+      </div>
+    </div>
+  );
 };
 
-const ConnectMenu = () => {
-  const connectOptions = ["metamask", "coinbase", "walletconnect"];
+type ConnectMenuProps = {
+  closeModal: () => void;
+};
+
+const ConnectMenu = ({ closeModal }: ConnectMenuProps) => {
+  console.log();
+
+  const connectOptions = ["metamask", "coinbase", "walletconnect"] as const;
   return (
     <div>
       {connectOptions.map((option) => (
-        <ConnectMenuOption key={option} text={option} />
+        <ConnectMenuOption key={option} text={option} onClick={closeModal} />
       ))}
     </div>
   );
