14fbad512e22fa175feb3f3a03190125d5f876fc
Razvan <not.obelix@gmail.com>
2022-10-22 20:14:53 +0300

    imported fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      roboto: ["Roboto", "sans-serif"],
+    },
   },
   plugins: [],
 };
