6106e85dc62b84e5d02eb12e9cd93bf8df97e305
Razvan <not.obelix@gmail.com>
2022-11-02 23:09:47 +0200

    added alchemy provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,9 +4,12 @@ import { configureChains } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
 import { WagmiConfig, createClient } from "wagmi";
 import { defaultChains } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { env } from "../env/client.mjs";
 
 const { provider, webSocketProvider } = configureChains(defaultChains, [
   publicProvider(),
+  alchemyProvider({ apiKey: env.NEXT_PUBLIC_ALCHEMY_API_KEY }),
 ]);
 
 const client = createClient({
