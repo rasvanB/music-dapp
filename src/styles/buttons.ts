db8c34e41b0acc626c22f25adb27e470eb579f28
Razvan <not.obelix@gmail.com>
2022-11-04 12:04:22 +0200

    changed button styles

--- src/styles/buttons.ts
+++ src/styles/buttons.ts
@@ -1,7 +1,7 @@
 import { cva } from "class-variance-authority";
 
 const button = cva("h-fit select-none p-2 px-6 font-bold", {
-  variants: { intent: { primary: "rounded-full bg-purple" } },
+  variants: { intent: { primary: "rounded-full bg-purple text-sm" } },
 });
 
 export default button;
