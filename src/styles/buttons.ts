67d99d6b87124ff06c42fd7a3cfd0c65c9a1ce46
Razvan <not.obelix@gmail.com>
2022-11-05 20:20:18 +0200

    button styles

--- src/styles/buttons.ts
+++ src/styles/buttons.ts
@@ -1,6 +1,6 @@
 import { cva } from "class-variance-authority";
 
-const button = cva("h-fit select-none p-2 px-6 font-bold", {
+const button = cva("h-fit select-none p-2 px-6 font-medium", {
   variants: { intent: { primary: "rounded-full bg-purple text-sm" } },
 });
 
