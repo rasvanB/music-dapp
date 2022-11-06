d81b3296487e23250c20de672dda8cd8043640ef
Razvan <not.obelix@gmail.com>
2022-11-06 18:29:06 +0200

    updated routes

--- src/pages/api/users/[address].ts
+++ src/pages/api/users/[address].ts
@@ -1,9 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "../../../server/db/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const { address } = req.query;
+  const address = req.query.address as string;
 
-  res.status(200).json({ message: address });
+  const user = await prisma.user.findUnique({
+    where: { address },
+  });
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
+  }
 };
 
 export default handler;
--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -1,7 +1,22 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { ZodError } from "zod";
+import { userCreation } from "../../../models/user";
+import { prisma } from "../../../server/db/client";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ message: "TEST" });
+  try {
+    switch (req.method) {
+      case "POST":
+        const user = userCreation.parse(req.body);
+        const createdUser = await prisma.user.create({ data: user });
+        return res.status(201).json(createdUser);
+      default:
+        return res.status(404).json({ message: "Method not supported" });
+    }
+  } catch (error) {
+    if (error instanceof ZodError)
+      return res.status(400).json({ error: "invalid data" });
+  }
 };
 
 export default handler;
