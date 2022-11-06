52a778d1a08a4043e7d64fc5d13cdd6704b1002e
Razvan <not.obelix@gmail.com>
2022-11-07 01:33:20 +0200

    catch errors to prevent req stalling

--- src/pages/api/users/index.ts
+++ src/pages/api/users/index.ts
@@ -13,19 +13,15 @@ export default async function handle(
         const user = userCreation.parse(req.body);
         const createdUser = await prisma.user.create({ data: user });
         return res.status(201).json(createdUser);
-      case "PUT":
-        const userData = userCreation.parse(req.body);
-        const updatedUser = await prisma.user.update({
-          where: { address: userData.address },
-          data: userData,
-        });
-        return res.status(201).json(updatedUser);
       default:
         return res.status(404).json({ message: "Method not supported" });
     }
   } catch (error) {
     if (error instanceof ZodError)
       return res.status(400).json({ error: "invalid data" });
-    else console.error(error);
+    else {
+      console.error(error);
+      return res.status(404).json({ error: "error" });
+    }
   }
 }
