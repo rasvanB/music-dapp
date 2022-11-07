281d8fdbba422e618dbb5a515cd8d1c7d422662a
Razvan <not.obelix@gmail.com>
2022-11-07 12:56:58 +0200

    added secret to sign jwt

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -3,6 +3,7 @@ import { z } from "zod";
 
 export const serverSchema = z.object({
   DATABASE_URL: z.string().url(),
+  SECRET: z.string(),
   NODE_ENV: z.enum(["development", "test", "production"]),
 });
 
