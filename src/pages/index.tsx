36b1b92a8202d6a94cbc46b086436109ab571209
Razvan <not.obelix@gmail.com>
2022-11-08 22:22:43 +0200

    logic to fetch user on page load

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,47 @@
+import { useAtom } from "jotai";
+import { decode, JwtPayload } from "jsonwebtoken";
 import type { NextPage } from "next";
-import { useState } from "react";
-import { useAccount, useConnect, useEnsName } from "wagmi";
+import { useEffect, useState } from "react";
+import { useConnect } from "wagmi";
 import ConnectMenu from "../components/connect-menu";
 import Modal from "../components/modal";
+import { getUserData, setBearerToken } from "../services/users";
 import button from "../styles/buttons";
+import { userAtom } from "../utils/store";
 
 const Home: NextPage = () => {
   const [isModalOpen, setModalOpen] = useState(false);
-  const { address, isConnected } = useAccount();
   const { connectors } = useConnect();
+  const [user, setUser] = useAtom(userAtom);
 
-  const { data: ensName } = useEnsName({ address });
+  useEffect(() => {
+    if (!user) {
+      const tokenAsJson = window.localStorage.getItem("auth:user");
+      if (tokenAsJson) {
+        const token = JSON.parse(tokenAsJson);
+        if (token) {
+          setBearerToken(token);
+          const decodedData = decode(token) as JwtPayload | null;
+          if (
+            decodedData &&
+            decodedData.exp &&
+            decodedData.publicAddress &&
+            Date.now() < decodedData.exp * 1000
+          ) {
+            getUserData(decodedData.publicAddress).then((user) => {
+              if (user) setUser(user);
+            });
+          } else window.localStorage.removeItem("auth:user");
+        }
+      } else setUser(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div className="relative flex h-[100vh] items-center justify-center bg-dark font-inter text-white ">
-      {isConnected ? (
-        <div>Connected to {ensName ?? address}</div>
+      {user ? (
+        <div>Connected to {user.address} YO</div>
       ) : (
         <button
           onClick={() => setModalOpen(true)}
