76ebb21a6af0e6518ac585ac1a2e0be454bfd3ac
Razvan <not.obelix@gmail.com>
2022-11-08 22:24:01 +0200

    set up service function to fetch auth user

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { User } from "../utils/store";
 import axios from "axios";
 import { userCreation } from "../models/user";
 
@@ -7,21 +7,41 @@ type BasicUser = {
   nonce: string;
 };
 
+let bearerToken = "";
+
+export const setBearerToken = (token: string) => {
+  bearerToken = token;
+};
+
 export const getUserBasicData = async (
   address: string
 ): Promise<BasicUser | undefined> => {
   try {
     const response = await axios
-      .get("/api/users/" + address)
+      .get<BasicUser>("/api/users/" + address)
       .then((res) => res.data);
     return response;
   } catch (error) {
-    if (error instanceof Error) {
-      throw error;
-    }
+    throw error;
   }
 };
 
+export const getUserData = async (
+  address: string
+): Promise<User | undefined> => {
+  try {
+    const user = await axios
+      .get<User>("/api/users/" + address, {
+        headers: { Authorization: "Bearer " + bearerToken },
+      })
+      .then((res) => res.data);
+    return user;
+  } catch (error) {
+    throw error;
+  }
+};
+
+//TODO: THIS SHOULD CHECK FOR ERRORS
 export const createUser = async (user: User): Promise<User> => {
   const createdUser = await axios
     .post("/api/users/", user)
@@ -29,15 +49,10 @@ export const createUser = async (user: User): Promise<User> => {
   return userCreation.parse(createdUser);
 };
 
-type AuthData = {
-  token: string;
-  user: User;
-};
-
 export const authUser = async (
   address: string,
   signature: string
-): Promise<AuthData | undefined> => {
+): Promise<{ user: User; token: string } | undefined> => {
   try {
     const authentificatedUser = await axios
       .post("/api/auth", { address, signature })
