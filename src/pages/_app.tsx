a4cac014ccffa78b3d8ca8d9e56132be9af91a5c
Razvan <not.obelix@gmail.com>
2022-11-09 19:42:33 +0200

    moved user fetching to app component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,11 @@ import { env } from "../env/client.mjs";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
+import { userAtom } from "../utils/store";
+import { getUserData, setBearerToken } from "../services/users";
+import { decode, JwtPayload } from "jsonwebtoken";
 
 const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
   publicProvider(),
@@ -37,6 +42,32 @@ const client = createClient({
 });
 
 const MyApp: AppType = ({ Component, pageProps }) => {
+  const [user, setUser] = useAtom(userAtom);
+
+  useEffect(() => {
+    if (!user) {
+      const tokenAsJson = window.localStorage.getItem("auth:user");
+      if (tokenAsJson) {
+        const token = JSON.parse(tokenAsJson);
+        if (token) {
+          setBearerToken(token);
+          const decodedData = decode(token) as JwtPayload | null;
+          if (
+            decodedData &&
+            decodedData.exp &&
+            decodedData.publicAddress &&
+            Date.now() < decodedData.exp * 1000
+          ) {
+            getUserData(decodedData.publicAddress).then((user) => {
+              if (user) setUser(user);
+            });
+          } else window.localStorage.removeItem("auth:user");
+        }
+      } else setUser(null);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <WagmiConfig client={client}>
       <Component {...pageProps} />
