c74dd0011dbb5e30aa582499cb916b38815ff88e
Razvan <not.obelix@gmail.com>
2022-11-12 00:11:37 +0200

    finished setting up user profile component

--- src/components/user-profile.tsx
+++ src/components/user-profile.tsx
@@ -3,18 +3,22 @@ import { userAtom } from "../utils/store";
 import Image from "next/image";
 import { generateIcon } from "../utils/identicon";
 
+// TODO: THINK IF USER SHOULD BE PROP INSTEAD OF USING JOTAI HOOK
 const UserProfile = () => {
   const [user] = useAtom(userAtom);
   if (!user) return null;
   return (
-    <div className="flex">
-      <Image
-        src={"data:image/svg+xml;base64," + generateIcon(user?.address)}
-        width={50}
-        height={50}
-        alt="profile_picture"
-      ></Image>
-      {user?.address}
+    <div className="flex items-center gap-3 rounded-full bg-[#1C1C1C] p-3 py-2 text-sm font-medium tracking-wider outline outline-1 outline-[#2c2c2c]">
+      <div className="flex">
+        <Image
+          src={"data:image/svg+xml;base64," + generateIcon(user?.address)}
+          width={25}
+          height={25}
+          className="rounded-full"
+          alt="profile_picture"
+        ></Image>
+      </div>
+      {`${user.address.slice(0, 6)}...${user.address.slice(-4)}`}
     </div>
   );
 };
