b528674abdd1fc720466230bf61d0de23d66c075
Razvan <not.obelix@gmail.com>
2022-11-13 00:33:16 +0200

    restyled modal

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -11,9 +11,9 @@ type ModalProps = {
 const Modal = ({ setModalOpen, children, title }: ModalProps) => {
   return (
     <div className="fixed top-0 flex h-[100vh] w-[100vw] items-center justify-center bg-black bg-opacity-30">
-      <div className="relative h-fit w-fit min-w-[220px] rounded-lg bg-[#1A1B1F] px-5 pb-5 outline outline-1 outline-zinc-800">
-        <div className="mt-3 mb-3 flex items-center">
-          <h1 className="text-md mr-9 font-semibold text-gray-200">{title}</h1>
+      <div className="relative h-fit w-fit min-w-[220px] rounded-lg bg-[#1A1B1F] outline outline-1 outline-zinc-800">
+        <div className="mt-3 mb-3 flex items-center px-4">
+          <h1 className="text-md mr-7 font-semibold text-gray-200">{title}</h1>
           <div
             className=" w-fit cursor-pointer rounded-lg p-1.5 duration-100 hover:scale-105 hover:bg-[#2c2d31]"
             onClick={() => setModalOpen(false)}
@@ -25,7 +25,7 @@ const Modal = ({ setModalOpen, children, title }: ModalProps) => {
           </div>
         </div>
         <Divider color="#2d2e33" width="1px" />
-        {children}
+        <div className="px-4 pb-3">{children}</div>
       </div>
     </div>
   );
