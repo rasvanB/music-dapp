88f2f6d66f84ab75762634eaa25ac0313b20c980
Razvan <not.obelix@gmail.com>
2022-11-24 13:56:23 +0200

    api route + page for testing

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,8 +1,10 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import getMetadata from "../../utils/metadata";
 
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  getMetadata();
   return res.status(200).json({ test: "test" });
 }
---
+++ src/pages/main/upload/index.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const Upload: NextPage = () => {
+  return (
+    <div>
+      <h1>Upload</h1>
+    </div>
+  );
+};
+
+export default Upload;
