bdfb33ea912ba7020fd9f9ec8c4736490507bb09
Razvan <not.obelix@gmail.com>
2022-11-24 22:54:59 +0200

    worked on metadata extract function (still static)

---
+++ src/models/getMetadata.ts
@@ -0,0 +1,43 @@
+import { z } from "zod";
+
+export const maxSeconds = 60 * 30; // 30 minutes
+
+export const metadataObject = z.object({
+  format: z.object({
+    bitrate: z.number({
+      required_error: "Bitrate is required",
+    }),
+    duration: z
+      .number({
+        required_error: "Duration is required",
+        invalid_type_error: "Duration must be a number",
+      })
+      .positive()
+      .refine(
+        (arg) => {
+          return arg <= maxSeconds;
+        },
+        {
+          message: `Duration must be less than ${maxSeconds} seconds`,
+        }
+      ),
+  }),
+  common: z.object({
+    title: z.string({
+      required_error: "Title is required",
+    }),
+    artist: z.string({
+      required_error: "Artist is required",
+    }),
+    genre: z
+      .array(z.string({}), {
+        required_error: "Genre is required",
+      })
+      .nonempty(),
+    date: z.string({
+      required_error: "Date is required",
+    }),
+  }),
+});
+
+export type Metadata = z.infer<typeof metadataObject>;
