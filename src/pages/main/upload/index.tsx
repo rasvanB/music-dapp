f85ae4b46b43bcd6be4bd48f0a5766985ed9de10
rasvanB <not.obelix@gmail.com>
2022-11-26 21:15:28 +0200

    added file upload input + handler for testing

--- src/pages/main/upload/index.tsx
+++ src/pages/main/upload/index.tsx
@@ -1,9 +1,23 @@
 import { NextPage } from "next";
+import { getMetadataFromFile } from "../../../services/files";
 
 const Upload: NextPage = () => {
+  const updateHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      try {
+        const metadata = await getMetadataFromFile(file);
+        console.log(metadata);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div>
       <h1>Upload</h1>
+      <input type="file" accept=".mp3" onChange={updateHandler} />
     </div>
   );
 };
