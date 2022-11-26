7645e3b5e4f2863df79fa55e8decfeb3a591208c
rasvanB <not.obelix@gmail.com>
2022-11-26 22:55:01 +0200

    changed metadata format

--- src/models/metadata.ts
+++ src/models/metadata.ts
@@ -3,41 +3,40 @@ import { z } from "zod";
 export const maxSeconds = 60 * 30; // 30 minutes
 
 export const metadataObject = z.object({
-  format: z.object({
-    bitrate: z.number({
-      required_error: "Bitrate is required",
-    }),
-    duration: z
-      .number({
-        required_error: "Duration is required",
-        invalid_type_error: "Duration must be a number",
-      })
-      .positive()
-      .refine(
-        (arg) => {
-          return arg <= maxSeconds;
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title must be a string",
+  }),
+  artist: z.string({
+    required_error: "Artist is required",
+    invalid_type_error: "Artist must be a string",
+  }),
+  genre: z.string({
+    required_error: "Genre is required",
+    invalid_type_error: "Genre must be a string",
+  }),
+  year: z.string({
+    required_error: "Year is required",
+    invalid_type_error: "Year must be a string",
+  }),
+  TDAT: z.object(
+    {
+      id: z.string(),
+      data: z.string().refine(
+        (data) => {
+          const date = new Date(data);
+          return date instanceof Date && !isNaN(date.getTime());
         },
         {
-          message: `Duration must be less than ${maxSeconds} seconds`,
+          message: "Date is invalid",
         }
       ),
-  }),
-  common: z.object({
-    title: z.string({
-      required_error: "Title is required",
-    }),
-    artist: z.string({
-      required_error: "Artist is required",
-    }),
-    genre: z
-      .array(z.string({}), {
-        required_error: "Genre is required",
-      })
-      .nonempty(),
-    year: z.number({
-      required_error: "Year is required",
-    }),
-  }),
+    },
+    {
+      required_error: "Date is required",
+      invalid_type_error: "Date must be a string in the format YYYY-MM-DD",
+    }
+  ),
 });
 
 export type Metadata = z.infer<typeof metadataObject>;
