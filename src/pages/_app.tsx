b4bc0f55ce34dbc0a1853a546d17a449075f8611
rasvanB <not.obelix@gmail.com>
2022-11-28 17:10:17 +0200

    reorganized imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
-import "../styles/globals.css";
 import type { AppType } from "next/dist/shared/lib/utils";
-import { configureChains } from "wagmi";
 import { publicProvider } from "wagmi/providers/public";
-import { WagmiConfig, createClient } from "wagmi";
-import { defaultChains } from "wagmi";
+import {
+  WagmiConfig,
+  createClient,
+  defaultChains,
+  configureChains,
+} from "wagmi";
 import { alchemyProvider } from "wagmi/providers/alchemy";
-import { env } from "../env/client.mjs";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
 import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
-import { useEffect } from "react";
-import { useAtom } from "jotai";
+import { env } from "../env/client.mjs";
 import { userAtom } from "../utils/store";
 import { getUserData, setBearerToken } from "../services/users";
+import { useEffect } from "react";
+import { useAtom } from "jotai";
 import { decode, JwtPayload } from "jsonwebtoken";
+import "../styles/globals.css";
 
 const { chains, provider, webSocketProvider } = configureChains(defaultChains, [
   publicProvider(),
