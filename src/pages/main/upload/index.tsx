c5dda9164d594d1f83007447c4869f6971f76272
rasvanB <not.obelix@gmail.com>
2022-12-07 22:28:47 +0200

    display metadata in upload page

--- src/pages/main/upload/index.tsx
+++ src/pages/main/upload/index.tsx
@@ -1,17 +1,21 @@
 import { NextPage } from "next";
+import { useState } from "react";
+import { Metadata } from "../../../models/metadata";
 import { getMetadataFromFile } from "../../../utils/getMetadata";
 import ipfs from "../../../utils/ipfs/client";
 
 const Upload: NextPage = () => {
+  const [metadata, setMetadata] = useState<Metadata>();
+
   const updateHandler = async (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
       const file = e.target.files[0];
       try {
         const metadata = await getMetadataFromFile(file);
-        const result = await ipfs.add(file);
-        // DISPLAY METADATA AND IPFS HASH
+        setMetadata(metadata);
+        await ipfs.add(file);
       } catch (error) {
-        // ALERT COMPONENT UPDATE HERE
+        console.log(error);
       }
     }
   };
@@ -19,8 +23,19 @@ const Upload: NextPage = () => {
   return (
     <div>
       <h1>Upload</h1>
-      {/*TODO: JSMEDIATAGS ONLY SUPPORTS MP3, SHOULD ADD SUPPORT FOR FLAC TOO */}
       <input type="file" accept="audio/mp3" onChange={updateHandler} />
+      <section>
+        <h2>Metadata</h2>
+        {metadata ? (
+          <ul>
+            {Object.entries(metadata).map(([key, value]) => (
+              <li key={key}>{`${key} ${value}`}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No metadata available</p>
+        )}
+      </section>
     </div>
   );
 };
