71035c08d6786001d4dfc6b24b93f6c4b8c08a62
rasvanB <not.obelix@gmail.com>
2022-12-20 20:55:57 +0200

    return type

--- src/utils/identicon.ts
+++ src/utils/identicon.ts
@@ -1,6 +1,6 @@
 import makeBlockie from "ethereum-blockies-base64";
 
-export const generateIcon = (hash: string) => {
+export const generateIcon = (hash: string): string => {
   const data = makeBlockie(hash);
   return data;
 };
